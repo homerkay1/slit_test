93fbd4272ed4778726df9be7bc4f0e2c3810545b
homerkay1 <39789003+homerkay1@users.noreply.github.com>
2023-07-10 01:17:03 +0000

    adding requirements and adding multi file loader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,22 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
 
-st.write('Hello world!')
+import pytesseract
+import shutil
+import os
+import random
+from PIL import Image
+
+st.write('This is Auto Doc Detection for Mortgage Lenders')
+
+# TODO does this load on first use? 
+uploaded_files = st.file_uploader("Choose a .png or .jpg file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    # TODO Check for PDF Version Type
+    extractedInformation = pytesseract.image_to_string(Image.open(uploaded_file))
+    print(extractedInformation)
+    st.write(extractedInformation)
+    st.write('Doneee')
+    # TODO Connect to hugchat to interpret data
+
